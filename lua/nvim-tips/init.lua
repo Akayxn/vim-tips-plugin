b3a494b28654ba16ac340d9bc2caf1369d00f203
Akayxn <yujansharma20621129@gmail.com>
2026-05-11 21:51:08 +0545

    added new tips related to vim motions

--- lua/nvim-tips/init.lua
+++ lua/nvim-tips/init.lua
@@ -1,23 +1,39 @@
 local tipsModule = require("nvim-tips.tips")
 
 local function getCategoryItems(categoryName)
-  for index, value in ipairs(tipsModule.tips[categoryName]) do
-    print(value)
-  end
+	for index, value in ipairs(tipsModule.tips[categoryName]) do
+		print(value)
+	end
 end
 
 vim.api.nvim_create_user_command("TipNormal", function()
-  getCategoryItems("normal")
+	getCategoryItems("normal")
 end, {})
 
 vim.api.nvim_create_user_command("TipVisual", function()
-  getCategoryItems("visual")
+	getCategoryItems("visual")
 end, {})
 
 vim.api.nvim_create_user_command("TipDelete", function()
-  getCategoryItems("delete")
+	getCategoryItems("delete")
 end, {})
 
 vim.api.nvim_create_user_command("TipInsert", function()
-  getCategoryItems("insert")
+	getCategoryItems("insert")
+end, {})
+
+vim.api.nvim_create_user_command("TipFold", function()
+	getCategoryItems("fold")
+end, {})
+
+vim.api.nvim_create_user_command("TipMarks", function()
+	getCategoryItems("marks")
+end, {})
+
+vim.api.nvim_create_user_command("TipMacros", function()
+	getCategoryItems("macros")
+end, {})
+
+vim.api.nvim_create_user_command("TipSearch", function()
+	getCategoryItems("search")
 end, {})
--- lua/nvim-tips/tips.lua
+++ lua/nvim-tips/tips.lua
@@ -1,37 +1,61 @@
 local M = {}
 
 M.tips = {
-  normal = {
-    "h j k l = left,down,up,right",
-    "w = jump to next word",
-    "b = jump back a word",
-    "e = jump to end of the word",
-    "gg = go to top of file , G = go to bottom of the file",
-    "Ctrl+d = scroll down half page , Ctrl+u = scroll up half page",
-    "dd = delete line , yy = yank line , p = paste",
-    "u = undo , Ctrl+r = redo",
-    'ci" = change inside quotes, ci( = change inside brackets',
-    "% = jump to matching bracket",
-    "* = search word under cursor",
-    "zz = center screen on cursor",
-  },
-  visual = {
-    "v = character select , V = line select , Ctrl + v = block select",
-    "y = yank selected , d = delete selected",
-    "> = indent , < = unindent",
-    "~ = toggle case",
-  },
-  delete = {
-    "dw = delete word, d$ delete to end of line",
-    "diw = delete inner word, daw = delete a word + space",
-    "cw = change word, C = change to end of line",
-    "x = delete character",
-  },
-  insert = {
-    "i = insert before , a = insert after",
-    "I = insert at line start, A = insert at line end",
-    "o = new line below , O = new line above ",
-  },
+	normal = {
+		"h j k l = left,down,up,right",
+		"w = jump to next word",
+		"b = jump back a word",
+		"e = jump to end of the word",
+		"gg = go to top of file , G = go to bottom of the file",
+		"Ctrl+d = scroll down half page , Ctrl+u = scroll up half page",
+		"dd = delete line , yy = yank line , p = paste",
+		"u = undo , Ctrl+r = redo",
+		'ci" = change inside quotes, ci( = change inside brackets',
+		"% = jump to matching bracket",
+		"* = search word under cursor",
+		"zz = center screen on cursor",
+	},
+	visual = {
+		"v = character select , V = line select , Ctrl + v = block select",
+		"y = yank selected , d = delete selected",
+		"> = indent , < = unindent",
+		"~ = toggle case",
+	},
+	delete = {
+		"dw = delete word, d$ delete to end of line",
+		"diw = delete inner word, daw = delete a word + space",
+		"cw = change word, C = change to end of line",
+		"x = delete character",
+	},
+	insert = {
+		"i = insert before , a = insert after",
+		"I = insert at line start, A = insert at line end",
+		"o = new line below , O = new line above ",
+	},
+	fold = {
+		"zc = close Fold",
+		"zo = open Fold",
+		"za = toggle Fold ",
+		"zR = Open all folds",
+		"zM = close all folds",
+	},
+	marks = {
+		"ma = set mark a",
+		"`a = jump to mark a",
+		"'' = jump back to last position",
+	},
+	macros = {
+		"qa = record a macro to register a",
+		"q = stop recording",
+		"@a = play macro a",
+		"@@ = repeat last macro",
+	},
+	search = {
+		"/word = search forward",
+		"?word = search backward",
+		"n = next match, N previous match",
+		"%s/old/new/g = replace all",
+	},
 }
 
 return M
